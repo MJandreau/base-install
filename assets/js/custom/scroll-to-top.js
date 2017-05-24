7ba6301316b54da021e9160867d60ef1fbf75b8b
Mike Jandreau <mikejandreau@gmail.com>
2017-05-24 09:16:09 -0400

    Brought back vanilla JS scrolling got deleted - brought it back

--- assets/js/custom/scroll-to-top.js
+++ assets/js/custom/scroll-to-top.js
@@ -1,31 +1,148 @@
 
-// // fade #scroll-to-top button in on scroll
-// window.addEventListener("load", function(){
-//     scrollTo = function(element, to, duration) {
+function scrollToTop(options) {
+  this.options = options;
+  this.button = null;
+  this.stop = false;
+}
 
-//     if (duration < 0) return;
+scrollToTop.prototype.constructor = scrollToTop;
 
-//     var difference = to - element.scrollTop,
-//         perTick = difference / duration * 10;
+scrollToTop.prototype.createButton = function() {
+  this.button = document.createElement('button');
+  this.button.classList.add('scroll-to-top');
+  this.button.classList.add('scroll-to-top--hidden');
+  this.button.textContent = "Top";
+  document.body.appendChild(this.button);
+};
+  
+scrollToTop.prototype.init = function() {
+  this.createButton();
+  this.checkPosition();
+  this.click();
+  this.stopListener();
+};
 
-//         setTimeout(function() {
-//             element.scrollTop = element.scrollTop + perTick;
-//             if (element.scrollTop === to) return;
-//             scrollTo(element, to, duration - 10);
-//         }, 10);
-//     };
+scrollToTop.prototype.scroll = function() {
+  if (this.options.animate === false || this.options.animate === "false") {
+    this.scrollNoAnimate();
+    return;
+  }
+  if (this.options.animate == "normal") {
+    this.scrollAnimate();
+    return;
+  }
+  if (this.options.animate == "linear") {
+    this.scrollAnimateLinear();
+    return;
+  }
+};
 
-//     var scrollTopButton = document.getElementById("scroll-to-top");
+scrollToTop.prototype.scrollNoAnimate = function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
 
-//     document.addEventListener('scroll', function() {
-//         if (document.body.scrollTop > 100) {
-//             scrollTopButton.classList.add("scrolled");
-//         } else {
-//             scrollTopButton.classList.remove("scrolled");
-//         }
-//     });
+scrollToTop.prototype.scrollAnimate = function() {
+  if (this.scrollTop() > 0 && this.stop === false) {
+    setTimeout(function() {
+      this.scrollAnimate();
+      window.scrollBy(0, (-Math.abs(this.scrollTop())/this.options.normal.steps));
+    }.bind(this), (this.options.normal.ms));
+  }
+};
 
-//     scrollTopButton.addEventListener('click', function() {
-//         scrollTo(document.body, document.body.offsetTop, 400);
-//     });
-// });
+scrollToTop.prototype.scrollAnimateLinear = function() {
+  if (this.scrollTop() > 0 && this.stop === false) {
+    setTimeout(function() {
+      this.scrollAnimateLinear();
+      window.scrollBy(0, -Math.abs(this.options.linear.px));
+    }.bind(this), this.options.linear.ms);
+  }
+};
+
+scrollToTop.prototype.click = function() {
+  
+  this.button.addEventListener("click", function(e) {
+    e.stopPropagation();
+      this.scroll();
+  }.bind(this), false);
+  
+  this.button.addEventListener("dblclick", function(e) {
+    e.stopPropagation();
+      this.scrollNoAnimate();
+  }.bind(this), false);
+  
+};
+
+scrollToTop.prototype.hide = function() {
+  this.button.classList.add("scroll-to-top--hidden");
+};
+
+scrollToTop.prototype.show = function() {
+  this.button.classList.remove("scroll-to-top--hidden");
+};
+
+scrollToTop.prototype.checkPosition = function() {
+  window.addEventListener("scroll", function(e) {
+    if (this.scrollTop() > this.options.showButtonAfter) {
+      this.show();
+    } else {
+      this.hide();
+    }
+  }.bind(this), false);
+};
+
+scrollToTop.prototype.stopListener = function() {
+  
+  // stop animation on slider drag
+  var position = this.scrollTop();
+  window.addEventListener("scroll", function(e) {
+    if (this.scrollTop() > position) {
+      this.stopTimeout(200);
+    } else {
+      //...
+    }
+    position = this.scrollTop();
+  }.bind(this, position), false);
+
+  // stop animation on wheel scroll down
+  window.addEventListener("wheel", function(e) {
+    if(e.deltaY > 0) this.stopTimeout(200);
+  }.bind(this), false);
+};
+
+scrollToTop.prototype.stopTimeout = function(ms){
+   this.stop = true;
+         // console.log(this.stop); //
+   setTimeout(function() {
+     this.stop = false;
+           console.log(this.stop); //
+   }.bind(this), ms);
+};
+
+scrollToTop.prototype.scrollTop = function(){
+   var curentScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  return curentScrollTop;
+};
+
+
+
+// ------------------- USE EXAMPLE ---------------------
+// *Set options
+var options = {
+  'showButtonAfter': 200, // show button after scroling down this amount of px
+  'animate': "normal", // [false|normal|linear] - for false no aditional settings are needed
+  // easy out effect
+  'normal': { // applys only if [animate: normal] - set scroll loop "distanceLeft"/"steps"|"ms"
+    'steps': 15, // more "steps" per loop => slower animation
+    'ms': 1000/60 // less "ms" => quicker animation, more "ms" => snapy
+  },
+  // linear effect
+  'linear': { // applys only if [animate: linear] - set scroll "px"|"ms"
+    'px': 80, // more "px" => quicker your animation gets
+    'ms': 1000/60 // Less "ms" => quicker your animation gets, More "ms" =>
+  }, 
+};
+// *Create new scrollToTop and run it.
+var scroll = new scrollToTop(options);
+scroll.init();
