64cb3d8fe26ef9a86c4b483779bcae51162c8549
Mike Jandreau <mikejandreau@gmail.com>
2017-06-01 11:04:34 -0400

    Some housekeeping

--- footer.php
+++ footer.php
@@ -15,9 +15,9 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'baseinstall' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'baseinstall' ), 'WordPress' ); ?></a>
+			<span class="copyright">Copyright &copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?></span>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'baseinstall' ), 'baseinstall', '<a href="https://automattic.com/" rel="designer">Mike Jandreau</a>' ); ?>
+			<span class="designer">Designed by <a href="https://www.mikejandreau.net/" target="_blank">Mike Jandreau</a></span>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
