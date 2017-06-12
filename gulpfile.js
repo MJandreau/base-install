fb54f24cc013daa15fac9f572dc34b20c22f6e4d
Mike Jandreau <mikejandreau@gmail.com>
2017-06-12 13:43:04 -0400

    Updated code styles

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@
 // START Editing Project Variables.
 // Project related.
 var project                 = 'Base_Install'; // Project Name.
-var projectURL              = 'dev8'; // Project URL. Could be something like localhost:8888.
+var projectURL              = 'baseinstall'; // Project URL. Could be something like localhost:8888.
 var productURL              = './'; // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
 
 // Translation related.
