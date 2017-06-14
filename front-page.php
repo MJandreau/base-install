72621b24b455b53cdf0b43c197615e5753acfb3e
Mike Jandreau <mikejandreau@gmail.com>
2017-06-14 13:03:43 -0400

    Created front page template

---
+++ front-page.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Base_Install
+ */
+
+?>
+
+<div class="triple-feature">
+	<div class="container">
+		<div class="row">
+			<div class="column four-md">
+				<div class="featured">
+					<h3>bloop</h3>
+				</div>
+			</div>
+			<div class="column four-md">
+				<div class="featured">
+					<h3>bloop</h3>
+				</div>
+			</div>
+			<div class="column four-md">
+				<div class="featured">
+					<h3>bloop</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+
+<h1>bloop</h1>
+
+	<?php
+	while ( have_posts() ) : the_post();
+
+		get_template_part( 'template-parts/content', 'page' );
+
+		// If comments are open or we have at least one comment, load up the comment template.
+		// if ( comments_open() || get_comments_number() ) :
+		// 	comments_template();
+		// endif;
+
+	endwhile; // End of the loop.
+	?>
+
+</div>
