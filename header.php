814aac4ee1e25602d8fafa5307a57b53675921c2
Mike Jandreau <mikejandreau@gmail.com>
2017-06-15 00:09:58 -0400

    Header markup and style update

--- header.php
+++ header.php
@@ -16,13 +16,21 @@
 
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><?php // .site-branding ?>
+
+<?php // site description, delete later ?>
+<?php
+// $description = get_bloginfo( 'description', 'display' );
+// if ( $description || is_customize_preview() ) : ?>
+<!-- <p class="site-description"> -->
+<?php 
+// echo $description; /* WPCS: xss ok. */ 
+?>
+<!-- </p> -->
+<?php 
+// endif; 
+?>
+
+		</div>
 
 		<button class="menu-toggle" tabindex="0" aria-label="Menu" aria-controls="primary-menu"><?php esc_html_e( 'Menu', 'baseinstall' ); ?><span>toggle menu</span></button>
 
