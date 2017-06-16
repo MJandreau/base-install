f89638e6d8b874b9d6a0c187ff8a39953c1efa40
Mike Jandreau <mikejandreau@gmail.com>
2017-06-16 14:48:12 -0400

    Added featured component

---
+++ template-parts/featured.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The feature section for our theme
+ *
+ * This is the template that displays below the .hero (optional)
+ * if you don't want/need it, comment out or delete get_template_part('feature'); in base.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Base_Install
+ */
+?>
+
+<div class="triple-feature">
+	<div class="container">
+		<div class="row">
+			<div class="column four-md">
+				<div class="featured">
+					<h3>bloop</h3>
+				</div>
+			</div>
+			<div class="column four-md">
+				<div class="featured">
+					<h3>bloop</h3>
+				</div>
+			</div>
+			<div class="column four-md">
+				<div class="featured">
+					<h3>bloop</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
