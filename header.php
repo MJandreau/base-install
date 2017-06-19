dac2f3d77a7e48f299c3df6771f3325b8bb24fa9
Mike Jandreau <mikejandreau@gmail.com>
2017-06-19 15:39:58 -0400

    Markup cleanup

--- header.php
+++ header.php
@@ -11,29 +11,11 @@
 ?>
 
 <header id="masthead" class="site-header">
-
 	<div class="site-navbar">
-
 		<div class="site-branding">
 			<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-
-<?php // site description, delete later ?>
-<?php
-// $description = get_bloginfo( 'description', 'display' );
-// if ( $description || is_customize_preview() ) : ?>
-<!-- <p class="site-description"> -->
-<?php 
-// echo $description; /* WPCS: xss ok. */ 
-?>
-<!-- </p> -->
-<?php 
-// endif; 
-?>
-
 		</div>
-
 		<button class="menu-toggle" tabindex="0" aria-label="Menu" aria-controls="primary-menu"><?php esc_html_e( 'Menu', 'baseinstall' ); ?><span>toggle menu</span></button>
-
 		<nav id="site-navigation" class="main-navigation">
 			<h2 class="screen-reader-text">Main Navigation</h2>
 			<?php wp_nav_menu( array(
@@ -43,17 +25,5 @@
 				'menu_id' => 'primary-menu'
 			 ) ); ?>
 		</nav>
-
 	</div>
-
 </header>
-
-
-
-
-
-
-
-
-
-
