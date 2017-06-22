48af79da1f56eb51b51ff5d9e9e52058a20228a8
Mike Jandreau <mikejandreau@gmail.com>
2017-06-21 22:15:02 -0400

    Updated page titles in hero

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -18,13 +18,13 @@
 
 			<?php if ( is_front_page() ) : ?>
 				<div class="call-to-action">
-					<h2>Base Install Four</h2>
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 					<p>Base Install is a starter theme for WordPress development based on <a href="http://underscores.me/" target="_blank">Underscores</a>. It features a <a href="http://scribu.net/wordpress/theme-wrappers.html" target="_blank">theme wrapper</a>, a <a href="http://getskeleton.com/" target="_blank">responsive grid</a>, and a powerful <a href="https://labs.ahmadawais.com/WPGulp/" target="_blank">Gulp</a> workflow.</p>
 					<a href="https://github.com/mikejandreau/Base-Install" target="_blank">View on GitHub</a>
 				</div>
 				
 			<?php elseif ( is_single() ) : ?>
-				<?php the_title( '<h2 class="entry-title"><span class="screen-reader-text">Article Title: </span>', '</h2>' ); ?>
+				<?php the_title( '<h1 class="entry-title"><span class="screen-reader-text">Article Title: </span>', '</h1>' ); ?>
 				<div class="blog-author-date">
 					<span class="blog-date">Posted on <?php echo get_the_date('M j, Y'); ?></span>
 					<?php 
@@ -44,32 +44,32 @@
 					$first_name = get_the_author_meta('first_name',$user_id);
 					$last_name = get_the_author_meta('last_name',$user_id);
 					$full_name = "{$first_name} {$last_name}";
-					echo '<h2 class="entry-title">Posts by ' . $full_name . ' </h2>';
+					echo '<h1 class="entry-title">Posts by ' . $full_name . ' </h1>';
 				?>
 
 			<?php elseif ( is_category() ) : ?>
-				<?php single_cat_title( '<h2 class="entry-title">Category: ', '</h2>' ); ?>
+				<?php single_cat_title( '<h1 class="entry-title">Category: ', '</h1>' ); ?>
 
 			<?php elseif ( is_tag() ) : ?>
-				<?php single_tag_title( '<h2 class="entry-title">Tag: ', '</h2>' ); ?>
+				<?php single_tag_title( '<h1 class="entry-title">Tag: ', '</h1>' ); ?>
 
 			<?php elseif ( is_month() ) : ?>
-				<?php the_archive_title( '<h2 class="entry-title">', '</h2>' ); ?>
+				<?php the_archive_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 			<?php elseif ( is_search() ) : ?>
-				<?php echo '<h2 class="entry-title">Search results for: ' . get_search_query() .  '</h2>'; ?>
+				<?php echo '<h1 class="entry-title">Search results for: ' . get_search_query() .  '</h1>'; ?>
 
 			<?php elseif ( is_home() ) : ?>
-				<h2>Blog</h2>
+				<h1>Blog</h1>
 
 			<?php elseif ( is_404() ) : ?>
-				<h2>Oops! That page can&rsquo;t be found.</h2>
+				<h1>Oops! That page can&rsquo;t be found.</h1>
 
 			<?php elseif ( is_page_template( 'contact.php' ) ) : ?>
-				<h2>Get in Touch</h2>
+				<h1>Get in Touch</h1>
 
 			<?php else : ?>
-				<?php the_title( '<h2 class="entry-title"><span class="screen-reader-text">Article Title: </span>', '</h2>' ); ?>
+				<?php the_title( '<h1 class="entry-title"><span class="screen-reader-text">Article Title: </span>', '</h1>' ); ?>
 
 			<?php endif; ?>
 
