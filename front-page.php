5ff7100405e0aa82339764579b7cfff0ed9e912b
Mike Jandreau <mikejandreau@gmail.com>
2017-06-29 13:45:55 -0400

    Minor tweaks to front page blog styles

--- front-page.php
+++ front-page.php
@@ -100,80 +100,85 @@
 		</div>
 	</div>
 
+	<div class="blog-section">
+		<div class="container">
 
+			<h2>Blog</h2>
+			<p>Integer sollicitudin turpis sit amet consectetur sodales. Donec rutrum hendrerit bibendum. Mauris et elementum mauris. Cras malesuada tortor in erat bibendum, vel volutpat sem semper. Donec dui sapien, sagittis eget euismod at, tincidunt quis mauris. Fusce a tempus lacus. Aliquam rutrum accumsan mi non lobortis.</p>
 
+			<div class="row">
+				<?php
+					$counter = 6; // Number of posts to pull
+					$recentPosts = new WP_Query(array(
+						'showposts' => $counter, 
+						'offset' => 0,  // Set this to 1 to skip over first post, 2 to skip the first two, etc.
+						'order' => 'DESC', // Puts new posts first, to put oldest posts first, change to 'ASC'
+						'post__not_in' => get_option("sticky_posts"),
+					));
+				?>
 
+				<?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+				<div class="column md-6 lg-4 box<?php echo $counter--; ?>">
+					<div class="blog-feature">
+						<div class="blog-image">
+							<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' ); ?>
+							<a href="<?php echo get_permalink( get_the_ID() );?>" style="background-image: url('<?php echo $thumb['0'];?>')"></a>
+						</div>
+						<div class="blog-text">
+							<h3><a href="<?php echo get_permalink( get_the_ID() );?>"><?php echo get_the_title(); ?></a></h3>
+							<div class="blog-author-date">
+								<?php 
+									// gets post date and bullet separator
+									echo '<span class="blog-date">' . get_the_date('M j, Y') . '</span><span class="blog-bullet">&nbsp;&bull;&nbsp;</span>'; 
 
+									// gets author name
+									$fname = get_the_author_meta('first_name');
+									$lname = get_the_author_meta('last_name');
+									$full_name = '';
 
+									if( empty($fname)){
+										$full_name = $lname;
+									} elseif( empty( $lname )){
+										$full_name = $fname;
+									} else {
+										// both first name and last name are present
+										$full_name = "{$fname} {$lname}";
+									}
+									echo '<span class="blog-author">By ' . $full_name . '</span>'; 
+								?>
+							</div>
+							<?php the_excerpt(); ?>
+							<a class="button" href="<?php echo get_permalink( get_the_ID() );?>">Read More</a>
+						</div>
+					</div>
+				</div>
+				<?php endwhile; ?>
+			</div>
+			<?php echo '<a class="button button-primary" href="' . get_permalink( get_option( 'page_for_posts' ) ) . '">View More Posts</a>'; ?>
+		</div>
+	</div>
 
-
-<div class="blog-section">
-    <div class="container">
-
-        <h2>Blog</h2>
-        <p>Integer sollicitudin turpis sit amet consectetur sodales. Donec rutrum hendrerit bibendum. Mauris et elementum mauris. Cras malesuada tortor in erat bibendum, vel volutpat sem semper. Donec dui sapien, sagittis eget euismod at, tincidunt quis mauris. Fusce a tempus lacus. Aliquam rutrum accumsan mi non lobortis.</p>
-
-        <div class="row">
-            <?php
-                $counter = 6; // Number of posts to pull
-				$recentPosts = new WP_Query(array(
-					'showposts' => $counter, 
-					'offset' => 0,  // Set this to 1 to skip over first post, 2 to skip the first two, etc.
-					'order' => 'DESC', // Puts new posts first, to put oldest posts first, change to 'ASC'
-					'post__not_in' => get_option("sticky_posts"),
-				));
-            ?>
-
-            <?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
-            <div class="column sm-6 lg-4 box<?php echo $counter--; ?>">
-                <div class="blog-feature">
-                    <div class="blog-image">
-                        <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );?>
-                        <a href="<?php echo get_permalink( get_the_ID() );?>" style="background-image: url('<?php echo $thumb['0'];?>')"></a>
-                    </div>
-                    <div class="blog-text">
-                        <h3><a href="<?php echo get_permalink( get_the_ID() );?>"><?php echo get_the_title(); ?></a></h3>
-                        <div class="blog-author-date">
-
-                            <!-- <span class="blog-date"> -->
-                                <?php 
-                                	// echo get_the_date('M j, Y'); 
-                                ?>
-                            <!-- </span> -->
-
-                            <!-- <span class="blog-bullet">•</span> -->
-
-                            <!-- <span class="blog-author">By  -->
-
-                                <?php
-                                    // $fname = get_the_author_meta('first_name');
-                                    // $lname = get_the_author_meta('last_name');
-                                    // $full_name = '';
-
-                                    // if( empty($fname)){
-                                    //     $full_name = $lname;
-                                    // } elseif( empty( $lname )){
-                                    //     $full_name = $fname;
-                                    // } else {
-                                    //     //both first name and last name are present
-                                    //     $full_name = "{$fname} {$lname}";
-                                    // }
-                                    // echo $full_name;
-                                ?>
-                            </span>
-                        </div>
-                        <?php the_excerpt(); ?>
-                        <a class="button" href="<?php echo get_permalink( get_the_ID() );?>">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <?php endwhile; ?>
-        </div>
-        <?php echo '<a class="button button-primary" href="' . get_permalink( get_option( 'page_for_posts' ) ) . '">View More Posts</a>'; ?>
-    </div>
-</div>
-
-
+	<div class="triple-feature">
+		<div class="container">
+			<div class="row">
+				<div class="column sm-4">
+					<div class="featured">
+						<h3>bloop</h3>
+					</div>
+				</div>
+				<div class="column sm-4">
+					<div class="featured">
+						<h3>bloop</h3>
+					</div>
+				</div>
+				<div class="column sm-4">
+					<div class="featured">
+						<h3>bloop</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 <?php endif; ?>
 
